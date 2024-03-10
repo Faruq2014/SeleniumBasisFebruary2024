c4663f297ef8b3566896a4a674af459a56bc9213
Mohammed Molla <mjmolla@gmail.com>
2024-03-10 14:37:36 -0400

    base test

---
+++ src/test/java/base/BaseTest.java
@@ -0,0 +1,39 @@
+package base;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class BaseTest {
+	
+	public WebDriver driver;
+	String browser="firefox";
+	
+	public WebDriver openApplication() {
+		
+		if (browser.equalsIgnoreCase("chrome"))	{
+			driver= new ChromeDriver();
+		}else if(browser.equalsIgnoreCase("firefox")) {
+			driver= new FirefoxDriver();
+		}else if (browser.equalsIgnoreCase("edge")) {
+			driver= new EdgeDriver();
+		}else {
+			System.out.println("Sorry we do not cover that browser");
+		}
+		
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://www.facebook.com/");
+		
+		return driver;
+		
+	}
+	
+	
+	public void closeApplication() {
+		driver.quit();
+	}
+}
--- src/test/java/webDriverCommand/WebdriverMethods.java
+++ src/test/java/webDriverCommand/WebdriverMethods.java
@@ -1,9 +1,13 @@
 package webDriverCommand;
 
+
+
+
 import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 
@@ -38,9 +42,40 @@ public class WebdriverMethods {
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.get("https://www.facebook.com/");
 		//how to print title from an application
-		String facbookTitle=driver.getTitle();
-		System.out.println(facbookTitle);
+		String expectedFacbookTitle=driver.getTitle();
+		System.out.println("the title of the facebook  "+expectedFacbookTitle);
+		//how to validate title
 		
+		     //positive = positive=positive
+		      //negative = negative=positive
+				//actual = expected=pass
+				//actual != expected=fail==defect
+				//Assertion package in TestNG
+		String actualTitle="Facebook - log in or sign up124";
+		//Assert.assertEquals(actualTitle, expectedFacbookTitle);
+		//Assert.assertSame(actualTitle, expectedFacbookTitle);
+		//Assert.assertTrue(actualTitle.equalsIgnoreCase(expectedFacbookTitle));
+		
+		
+		//positive data !=negative data
+		Assert.assertNotEquals(actualTitle, expectedFacbookTitle);
+		Assert.assertNotSame(actualTitle, expectedFacbookTitle);
+		Assert.assertFalse(actualTitle.equalsIgnoreCase(expectedFacbookTitle));
+	
+		
+		driver.quit();
+	}
+	
+	@Test
+	public void getFacebookUrl() {
+		driver= new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://www.facebook.com/");
+		String expectedUrl=driver.getCurrentUrl();
+		String actualUrl="https://www.facebook.com/";
+		Assert.assertEquals(expectedUrl, actualUrl);
+		driver.quit();
 		
 	}
 }
